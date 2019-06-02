48311b7f4cc4c179cf69716108b4a7d23de6accf
Alejandro1998elebebecito <49255376+Alejandro1998elebebecito@users.noreply.github.com>
2019-06-01 20:45:35 -0500

    cambia puerto

--- web/server.py
+++ web/server.py
@@ -19,4 +19,4 @@ def static_content(content):
 
 if __name__ == '__main__':
     app.secret_key = ".."
-    app.run(port=8080, threaded=True, host=('127.0.0.1'))
+    app.run(port=5000, threaded=True, host=('127.0.0.1'))
